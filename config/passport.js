1ddc6a85d936cd0c6d46bde3d6249f53f082d8cc
natansl <natansl@gmail.com>
2016-03-08 17:05:52 -0300

    Ajustando o aplicativo que foi colocado no heroku alterando chave de aplicacao do github

--- config/passport.js
+++ config/passport.js
@@ -5,30 +5,41 @@ var GitHubStrategy = require('passport-github').Strategy;
 var mongoose = require('mongoose');
 
 module.exports = function() {
-	
+
 	// Fazendo a ligação entre o model e o passport config
 	var Usuario = mongoose.model('Usuario');
 
 	// código anterior omitido
 	passport.use(new GitHubStrategy({
-		clientID: 'b360479ac66dee498617',
-		clientSecret: '478dedcba8079a4f85dab99c8d24f77ff36ca045',
-		callbackURL: 'http://localhost:3000/auth/github/callback'
-	},function(accessToken, refreshToken, profile, done) {
+
+		// MAQUINA LOCAL
+		//clientID: 'b360479ac66dee498617',
+		//clientSecret: '478dedcba8079a4f85dab99c8d24f77ff36ca045',
+		//callbackURL: 'http://localhost:3000/auth/github/callback'
+
+		// SERVIDOR HEROKU
+		clientID: '4e894100d7b987c05124',
+		clientSecret: '667d4e6fbfa647b57b1d62f643ab2acd0ad6ce45',
+		callbackURL: 'https://secure-bayou-39973.herokuapp.com/auth/github/callback'
+
+	}, function(accessToken, refreshToken, profile, done) {
 
 		Usuario.findOrCreate(
 
-			{ "login" : profile.username},
-			{ "nome" : profile.username},
+			{
+				"login": profile.username
+			}, {
+				"nome": profile.username
+			},
 			function(erro, usuario) {
-				if(erro) {
+				if (erro) {
 					console.log(erro);
 					return done(erro);
 				}
 				return done(null, usuario);
 			}
-			);
-		
+		);
+
 	}));
 
 	/*
@@ -37,18 +48,18 @@ module.exports = function() {
 		autenticação. Realizará a serialização apenas do
 		ObjectId do usuário na sessão.
 		*/
-		passport.serializeUser(function(usuario, done) {
-			done(null, usuario._id);
-		});
+	passport.serializeUser(function(usuario, done) {
+		done(null, usuario._id);
+	});
 
 
 	// Recebe o ObjectId do usuário armazenado na sessão
 	// Chamado a CADA requisição
 	passport.deserializeUser(function(id, done) {
 		Usuario.findById(id).exec()
-		.then(function(usuario) {
-			done(null, usuario);
-		});
+			.then(function(usuario) {
+				done(null, usuario);
+			});
 	});
 
 };
--- server.js
+++ server.js
@@ -4,15 +4,6 @@ var app = require('./config/express')(app);
 require('./config/passport')();
 require('./config/database')('mongodb://localhost/contatooh');
 
-http.createServer(app).listen(process.env.PORT || 5000, function() {
-	console.log('Express Server escutando na porta ' + process.env.PORT || 5000);
+http.createServer(app).listen(process.env.PORT || 3000, function() {
+	console.log('Express Server escutando na porta ' + process.env.PORT || 3000);
 });
-
-/*var http = require('http');
-var app = require('./config/express')();
-
-require('./config/database.js')('mongodb://localhost/contatooh');
-
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express Server escutando na porta ' + app.get('port'));
-});*/
