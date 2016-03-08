94895b2818adbfe2e87a5164f794e2bddc5604d1
natansl <natansl@gmail.com>
2016-03-08 18:54:51 -0300

    Alterando o arquivo contato.js para eliminar a autenticação na hora que o usuario loga

--- app/routes/contato.js
+++ app/routes/contato.js
@@ -1,38 +1,38 @@
 // Verifica se o usuario está logado
 function verificaAutenticacao(req, res, next) {
-if (req.isAuthenticated()) {
-return next();
-} else {
-res.status('401').json('Não autorizado');
-}
+	if (req.isAuthenticated()) {
+		return next();
+	} else {
+		res.status('401').json('Não autorizado');
+	}
 }
 
-module.exports = function(app){
-	
+module.exports = function(app) {
+
 	var controller = app.controllers.contato;
-	
+
 	console.log("Cheguei no controller" + controller);
 
 	app.route('/listageral')
-	.get(controller.listaContatos);
+		.get(controller.listaContatos);
 
 	app.route('/busca')
-	.get(verificaAutenticacao, controller.listaContatos)
-	.post(controller.pesquisaContato);
+		.get(controller.listaContatos)
+		.post(controller.pesquisaContato);
 
 	app.route('/cadastro_contato')
-	.get(controller.listaContatos);
+		.get(controller.listaContatos);
 
 	app.route('/login')
-	.get(controller.listaContatos);
-	
+		.get(controller.listaContatos);
+
 	app.route('/contatos')
-	.get(verificaAutenticacao, controller.listaContatos)
-	.post(controller.salvaContato);
-	
+		.get(verificaAutenticacao, controller.listaContatos)
+		.post(controller.salvaContato);
+
 	app.route('/contatos/:id')
-	.get(controller.obtemContato)
-	.delete(controller.removeContato);
+		.get(controller.obtemContato)
+		.delete(controller.removeContato);
 
 
 };
