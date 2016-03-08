507da25efc38dd4cc12bb20ed224398a0bb20655
natansl <natansl@gmail.com>
2016-03-08 18:08:29 -0300

    Alterando o arquivo server.js para conectar corretamente o serviço ao mongodb

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var http = require('http');
 var express = require('express');
 var app = require('./config/express')(app);
 require('./config/passport')();
-require('./config/database')('mongodb://localhost/contatooh');
+require('./config/database')(
+	'mongodb://heroku_drxf48f5:nslmjl@ds023448.mlab.com:23448/heroku_drxf48f5/contatooh'
+);
 
 http.createServer(app).listen(process.env.PORT || 3000, function() {
 	console.log('Express Server escutando na porta ' + process.env.PORT || 3000);
