695f8cb525c10d7bb9639a2e678bbaf5a2705c9a
natansl <natansl@gmail.com>
2016-03-08 18:02:13 -0300

    Ajustando a conexão com o banco de dados para conectar corretamente no heroku

--- consulta.js
+++ consulta.js
@@ -4,14 +4,21 @@ var ObjectID = require('mongodb').ObjectID;
 // ObjectID de algum contato existente
 var _idProcurado = new ObjectID('53ee689e89bd201218944bba');
 
-MongoClient.connect('mongodb://127.0.0.1:27017/contatooh',
+// MARQUINA LOCAL
+//MongoClient.connect('mongodb://127.0.0.1:27017/contatooh',
+
+// MAQUINA SERVIDOR HEROKU
+MongoClient.connect(
+	'mongodb://heroku_drxf48f5:nslmjl@ds023448.mlab.com:23448/heroku_drxf48f5',
 	function(erro, db) {
-		if(erro) throw err;
-		db.collection('contatos').findOne({_id : _idProcurado},
+		if (erro) throw err;
+		db.collection('contatos').findOne({
+				_id: _idProcurado
+			},
 			function(erro, contato) {
-				if(erro) throw err;
+				if (erro) throw err;
 				console.log(contato);
 			}
-			);
+		);
 	}
-	);
+);
