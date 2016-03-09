11bfd0b67c71643f461192e9a8bbf7edaa5f6d60
natansl <natansl@gmail.com>
2016-03-09 10:14:16 -0300

    Ajuste do arquivo do banco de dados

--- consulta.js
+++ consulta.js
@@ -9,7 +9,7 @@ var _idProcurado = new ObjectID('53ee689e89bd201218944bba');
 
 // MAQUINA SERVIDOR HEROKU
 MongoClient.connect(
-	'mongodb://heroku_drxf48f5:nslmjl@ds023448.mlab.com:23448/heroku_drxf48f5?authMode=scram-sha1',
+	'mongodb://heroku_drxf48f5:nslmjl1984@ds023448.mlab.com:23448/heroku_drxf48f5?authMode=scram-sha1',
 	function(erro, db) {
 		if (erro) throw err;
 		db.collection('contatos').findOne({
