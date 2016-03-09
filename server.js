1e2733b3ea0f460c912de7e63f57c80080238d45
natansl <natansl@gmail.com>
2016-03-08 22:02:04 -0300

    Alterando o arquivo contato.js para eliminar a autenticação na hora que o usuario loga

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = require('./config/express')(app);
 require('./config/passport')();
 require('./config/database')(
-	'mongodb://heroku_drxf48f5:nslmjl@ds023448.mlab.com:23448/heroku_drxf48f5'
+	'mongodb://heroku_drxf48f5:nslmjl@ds023448.mlab.com:23448/heroku_drxf48f5?authMode=scram-sha1'
 );
 
 http.createServer(app).listen(process.env.PORT || 3000, function() {
