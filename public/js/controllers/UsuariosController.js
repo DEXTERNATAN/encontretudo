21830da7309c147a5200b58001821b5a992f87ff
natansl <natansl@gmail.com>
2016-03-11 18:31:04 -0300

    Adicionado metodo de login de usuario e token de jwt

--- public/js/controllers/UsuariosController.js
+++ public/js/controllers/UsuariosController.js
@@ -1,60 +1,63 @@
-angular.module('contatooh').controller('UsuariosController', 
-	function(Usuario, $scope, $resource, $http, $location){
+angular.module('contatooh').controller('UsuariosController',
+	function(Usuario, $scope, $resource, $http, $location) {
 
 		$scope.usuarios = [];
-		$scope.mensagem = {texto: ''};
-		
+		$scope.mensagem = {
+			texto: ''
+		};
+
 		$scope.loginUsuario = function() {
-			var objeto = {username: $scope.usuario.login, senha: $scope.usuario.password};
-			console.log("Enviando: " + JSON.stringify(objeto));           
+			var objeto = {
+				username: $scope.usuario.login,
+				senha: $scope.usuario.password
+			};
+			console.log("Enviando: " + JSON.stringify(objeto));
 			// Usar para debugar a aplicação.
 			//debugger;
-			$http.post('/logar',objeto)
-				.success(function(data,status) {
+			$http.post('/logar', objeto)
+				.success(function(data, status) {
 
-					var token = btoa("{\"username\" : \"" 
-						+ $scope.usuario.login 
-						+ "\", \"senha\" : \"" 
-						+ $scope.usuario.password + "\"}");
+					var token = btoa("{\"username\" : \"" + $scope.usuario.login +
+						"\", \"senha\" : \"" + $scope.usuario.password + "\"}");
 
 					$http.defaults.headers.common.Authorization = 'BASIC ' + token;
 					sessionStorage.token = token;
 					console.log("Veio do servidor: " + data);
-					if (!data.length)
+					/*if (!data.length)
 	                	$scope.mensagem = {
 							texto: "Usuario e senha incorretos."
 						};
-					
+
 					if (data.length)
 	                	$location.path("/contatos");
-						
+						*/
 					//console.log("Login OK" + token);
 					//window.location.href = "/contatos";
 
 				})
 				.error(function(data, status) {
-					
+
 				});
 		};
 
 		// Função para cadastrar o usuario na base de dados
-		$scope.cadastraUsuario = function(){
-			
+		$scope.cadastraUsuario = function() {
+
 			console.log("Debug: Cheguei no login " + $scope.usuario.login);
 			console.log("Debug: Cheguei no password " + $scope.usuario.password);
 			console.log("Debug: Cheguei no email " + $scope.usuario.email);
-			
-			$http.post("/cadusuario", $scope.usuario).success(function (data) {
+
+			$http.post("/cadusuario", $scope.usuario).success(function(data) {
 				console.log("Sucesso" + data);
-				
-	                	$scope.mensagem = {
-							texto: "Usuario cadastrado com sucesso !!!"
-						};
-			}).error(function (data) {
+
+				$scope.mensagem = {
+					texto: "Usuario cadastrado com sucesso !!!"
+				};
+			}).error(function(data) {
 				console.log("Error" + $scope.usuario);
 			})
 
-			
+
 		}
 
 	});
