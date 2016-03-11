504ff234308b5238b5c914a4fb03c61028efc128
natansl <natansl@gmail.com>
2016-03-11 17:54:27 -0300

    Adicionado metodo de login de usuario e token de jwt

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var express = require('express');
 var app = require('./config/express')(app);
 require('./config/passport')();
 require('./config/database')(
-	
+
 	// MAQUINA LOCAL
-	'mongodb://localhost/contatooh'
-	
+	//'mongodb://localhost/contatooh'
+
 	// MAQUINA DO SERVIDOR HEROKU
-	//'mongodb://encontretudo:encontretudo@ds023448.mlab.com:23448/heroku_drxf48f5?authMode=scram-sha1'
+	'mongodb://encontretudo:encontretudo@ds023448.mlab.com:23448/heroku_drxf48f5?authMode=scram-sha1'
 );
 
 http.createServer(app).listen(process.env.PORT || 3000, function() {
@@ -34,4 +34,3 @@ require('./config/database.js')('mongodb://localhost/contatooh');
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express Server escutando na porta ' + app.get('port'));
 });*/
-
