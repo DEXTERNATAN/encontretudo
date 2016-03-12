139dc6a7b6c311d43d2d792f7e4b082fb0f587db
natansl <natansl@gmail.com>
2016-03-12 13:44:54 -0300

    Ajustada a função de login para pemitir cadastro de usuario

--- app/routes/usuario.js
+++ app/routes/usuario.js
@@ -1,22 +1,22 @@
 // Verifica se o usuario está logado
 function verificaAutenticacao(req, res, next) {
-if (req.isAuthenticated()) {
-return next();
-} else {
-res.status('401').json('Não autorizado');
-}
+	if (req.isAuthenticated()) {
+		return next();
+	} else {
+		res.status('401').json('Não autorizado');
+	}
 }
 
-module.exports = function(app){
-	
+module.exports = function(app) {
+
 	var controller = app.controllers.usuario;
-	
+
 	console.log("Cheguei no route do suario");
 
 	app.route('/cadusuario')
-	.post(controller.cadastraUsuario);
+		.post(controller.cadastraUsuario);
 
 	app.route('/logar')
-	.post(controller.loginUsuario);
-	
+		.post(controller.cadastraUsuario);
+
 };
