e98c9148b5bf12d336ffbe9cb0f66333fa4cc33f
natansl <natansl@gmail.com>
2016-03-12 14:01:19 -0300

    Ajustada a função de login para pemitir cadastro de usuario

--- public/js/controllers/UsuariosController.js
+++ public/js/controllers/UsuariosController.js
@@ -14,17 +14,28 @@
 				console.log("Enviando: " + JSON.stringify(objeto));
 				// Usar para debugar a aplicação.
 				//debugger;
-				$http.post("/cadusuario", $scope.usuario).success(function(data) {
-					console.log("Sucesso" + data);
+				$http.post('/logar', objeto).success(function(data, status) {
 
-					$scope.mensagem = {
-						texto: "Usuario cadastrado com sucesso !!!"
-					};
+						var token = btoa("{\"username\" : \"" + $scope.usuario.login +
+							"\", \"senha\" : \"" + $scope.usuario.password + "\"}");
 
-				}).error(function(data) {
-					console.log("Error" + $scope.usuario);
-				})
+						$http.defaults.headers.common.Authorization = 'BASIC ' + token;
+						sessionStorage.token = token;
+						console.log("Veio do servidor: " + data);
+						if (!data.length)
+							$scope.mensagem = {
+								texto: "Usuario e senha incorretos."
+							};
 
+						if (data.length)
+							$location.path("/contatos");
+						//console.log("Login OK" + token);
+						//window.location.href = "/contatos";
+
+					})
+					.error(function(data, status) {
+						console.log("Errorooooo" + data.mensagem);
+					});
 			};
 
 			// Função para cadastrar o usuario na base de dados
