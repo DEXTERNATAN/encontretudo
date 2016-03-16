5d71236195827de89f81fd3a09e749ca701dfb36
natansl <natansl@gmail.com>
2016-03-16 13:43:36 -0300

    Ajuste na tela de visualização e cadastro das empresas

--- consulta.js
+++ consulta.js
@@ -5,23 +5,23 @@ var ObjectID = require('mongodb').ObjectID;
 var _idProcurado = new ObjectID('53ee689e89bd201218944bba');
 
 MongoClient.connect(
+		
+	
+	MongoClient.connect(
+		// MAQUINA LOCAL
+		'mongodb://127.0.0.1:27017/contatooh',
 
-
-		MongoClient.connect(
-			// MAQUINA LOCAL
-			//'mongodb://127.0.0.1:27017/contatooh',
-
-			// MAQUINA SERVIDOR HEROKU
-			'mongodb://encontretudo:encontretudo@ds023448.mlab.com:23448/heroku_drxf48f5?authMode=scram-sha1',
-			function(erro, db) {
+		// MAQUINA SERVIDOR HEROKU
+		//'mongodb://encontretudo:encontretudo@ds023448.mlab.com:23448/heroku_drxf48f5?authMode=scram-sha1',
+	function(erro, db) {
+		if (erro) throw err;
+		db.collection('contatos').findOne({
+				_id: _idProcurado
+			},
+			function(erro, contato) {
 				if (erro) throw err;
-				db.collection('contatos').findOne({
-						_id: _idProcurado
-					},
-					function(erro, contato) {
-						if (erro) throw err;
-						console.log(contato);
-					}
-				);
+				console.log(contato);
 			}
 		);
+	}
+);
